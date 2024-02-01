968f161cbc66ff2025533807286fdca3b437fc8f
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-02-01 08:51:34 -0300

    add: create page home

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,21 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log("voce clicou em add")
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      
+      <TextInput style={styles.input} placeholder='Nome do participante' placeholderTextColor={"#6B6B6B"} />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
