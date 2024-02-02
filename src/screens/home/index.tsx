c8d09572d47b8914ca900683e608415dd9097d76
joaovtpereira <joaovitorpessoal2@gmail.com>
2024-02-01 21:36:35 -0300

    add component participant

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,23 +1,54 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { styles } from './styles';
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
-
   function handleParticipantAdd() {
-    console.log("voce clicou em add")
+    console.log("voce clicou em add");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log({ name });
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
-      
+
       <View style={styles.form}>
-      <TextInput style={styles.input} placeholder='Nome do participante' placeholderTextColor={"#6B6B6B"} />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6B6B6B"}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={["Joao Vitor", "Myllena"]}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista
+            de convidados
+          </Text>
+        )}
+      />
     </View>
   );
 }
